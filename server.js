44930bcdd62ae88598fbec3274fa3c3b007326be
Ahmed Gazdiev <gazahmed907@gmail.com>
2024-11-07 11:39:23 +0300

    instagram-server

--- server.js
+++ server.js
@@ -7,27 +7,26 @@ const SocketServer = require('./socketServer')
 const { ExpressPeerServer } = require('peer')
 const path = require('path')
 
-
 const app = express()
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}))
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+		credentials: true,
+	})
+)
 app.use(cookieParser())
-
 
 // Socket
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
 io.on('connection', socket => {
-    SocketServer(socket)
+	SocketServer(socket)
 })
 
 // Create peer server
 ExpressPeerServer(http, { path: '/' })
-
 
 // Routes
 app.use('/api', require('./routes/authRouter'))
@@ -37,27 +36,29 @@ app.use('/api', require('./routes/commentRouter'))
 app.use('/api', require('./routes/notifyRouter'))
 app.use('/api', require('./routes/messageRouter'))
 
-
 const URI = process.env.MONGODB_URL
-mongoose.connect(URI, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, err => {
-    if (err) throw err;
-    console.log('Connected to mongodb')
-})
+mongoose.connect(
+	URI,
+	{
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	err => {
+		if (err) throw err
+		console.log('Connected to mongodb')
+	}
+)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
+	app.use(express.static('client/build'))
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+	})
 }
-
 
 const port = process.env.PORT || 5000
 http.listen(port, () => {
-    console.log('Server is running on port', port)
+	console.log('Server is running on port', port)
 })
